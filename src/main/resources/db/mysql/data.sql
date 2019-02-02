8314d75ad0ab0d07e8641895b22227b9cf282287
David Pérez S <playeando.97@gmail.com>
2019-02-02 00:28:56 -0600

    Actualización de los scripts de BD
    Actualización de los scripts de BD:
    
    * Estructura
    * Datos

--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -1,53 +1,146 @@
-INSERT IGNORE INTO vets VALUES (1, 'James', 'Carter', '961 569 48 78', '', '1');
-INSERT IGNORE INTO vets VALUES (2, 'Helen', 'Leary', '961 445 74 14', '', '1');
-INSERT IGNORE INTO vets VALUES (3, 'Linda', 'Douglas', '961 121 23 25', '', '3');
-INSERT IGNORE INTO vets VALUES (4, 'Rafael', 'Ortega', '961 456 65 63', '', '3');
-INSERT IGNORE INTO vets VALUES (5, 'Henry', 'Stevens', '961 899 65 47', '', '2');
-INSERT IGNORE INTO vets VALUES (6, 'Sharon', 'Jenkins', '961 002 12 01', '', '1');
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-02-2019 a las 07:23:25
+-- Versión del servidor: 10.1.34-MariaDB
+-- Versión de PHP: 7.2.7
 
-INSERT IGNORE INTO specialties VALUES (1, 'radiology');
-INSERT IGNORE INTO specialties VALUES (2, 'surgery');
-INSERT IGNORE INTO specialties VALUES (3, 'dentistry');
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-INSERT IGNORE INTO vet_specialties VALUES (2, 1);
-INSERT IGNORE INTO vet_specialties VALUES (3, 2);
-INSERT IGNORE INTO vet_specialties VALUES (3, 3);
-INSERT IGNORE INTO vet_specialties VALUES (4, 2);
-INSERT IGNORE INTO vet_specialties VALUES (5, 1);
 
-INSERT IGNORE INTO types VALUES (1, 'cat');
-INSERT IGNORE INTO types VALUES (2, 'dog');
-INSERT IGNORE INTO types VALUES (3, 'lizard');
-INSERT IGNORE INTO types VALUES (4, 'snake');
-INSERT IGNORE INTO types VALUES (5, 'bird');
-INSERT IGNORE INTO types VALUES (6, 'hamster');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-INSERT IGNORE INTO owners VALUES (1, 'George', 'Franklin', '110 W. Liberty St.', 'Madison','USA', 'Ohio', '6085551023');
-INSERT IGNORE INTO owners VALUES (2, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie','USA', 'Wisconsin', '6085551749');
-INSERT IGNORE INTO owners VALUES (3, 'Eduardo', 'Rodriquez', '2693 Commerce St.', 'McFarland','USA', 'Wisconsin', '6085558763');
-INSERT IGNORE INTO owners VALUES (4, 'Harold', 'Davis', '563 Friendly St.', 'Eugene','USA', 'Oregón','6085553198');
-INSERT IGNORE INTO owners VALUES (5, 'Peter', 'McTavish', '2387 S. Fair Way', 'Madison','USA', 'Wisconsin','6085552765');
-INSERT IGNORE INTO owners VALUES (6, 'Jean', 'Coleman', '105 N. Lake St.', 'Mundelein','USA', 'Illinois', '6085552654');
-INSERT IGNORE INTO owners VALUES (7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona','USA', 'Lowa', '6085555387');
-INSERT IGNORE INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', 'USA', 'Tennesee','6085557683');
-INSERT IGNORE INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', 'USA', 'Wisconsin', '6085559435');
-INSERT IGNORE INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', 'USA', 'Wisconsin', '6085555487');
+--
+-- Base de datos: `petclinic`
+--
 
-INSERT IGNORE INTO pets VALUES (1, 'Leo', '2000-09-07', 1, 1);
-INSERT IGNORE INTO pets VALUES (2, 'Basil', '2002-08-06', 6, 2);
-INSERT IGNORE INTO pets VALUES (3, 'Rosy', '2001-04-17', 2, 3);
-INSERT IGNORE INTO pets VALUES (4, 'Jewel', '2000-03-07', 2, 3);
-INSERT IGNORE INTO pets VALUES (5, 'Iggy', '2000-11-30', 3, 4);
-INSERT IGNORE INTO pets VALUES (6, 'George', '2000-01-20', 4, 5);
-INSERT IGNORE INTO pets VALUES (7, 'Samantha', '1995-09-04', 1, 6);
-INSERT IGNORE INTO pets VALUES (8, 'Max', '1995-09-04', 1, 6);
-INSERT IGNORE INTO pets VALUES (9, 'Lucky', '1999-08-06', 5, 7);
-INSERT IGNORE INTO pets VALUES (10, 'Mulligan', '1997-02-24', 2, 8);
-INSERT IGNORE INTO pets VALUES (11, 'Freddy', '2000-03-09', 5, 9);
-INSERT IGNORE INTO pets VALUES (12, 'Lucky', '2000-06-24', 2, 10);
-INSERT IGNORE INTO pets VALUES (13, 'Sly', '2002-06-08', 1, 10);
+--
+-- Volcado de datos para la tabla `authorities`
+--
 
-INSERT IGNORE INTO visits VALUES (1, 7, '2010-03-04', 'rabies shot');
-INSERT IGNORE INTO visits VALUES (2, 8, '2011-03-04', 'rabies shot');
-INSERT IGNORE INTO visits VALUES (3, 8, '2009-06-04', 'neutered');
-INSERT IGNORE INTO visits VALUES (4, 7, '2008-09-04', 'spayed');
+INSERT IGNORE INTO `authorities` (`id`, `username`, `authority`) VALUES
+(1, 'david', 'USER'),
+(2, 'admin', 'USER');
+
+--
+-- Volcado de datos para la tabla `medicamento`
+--
+
+INSERT IGNORE INTO `medicamento` (`id`, `nombre`, `ingrediente_activo`, `presentacion`) VALUES
+(1, 'Paracetamol', 'Paracetamol', 'Pastillas');
+
+--
+-- Volcado de datos para la tabla `owners`
+--
+
+INSERT IGNORE INTO `owners` (`id`, `first_name`, `last_name`, `address`, `city`, `country`, `estado`, `telephone`) VALUES
+(1, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', 'USA', 'Ohio', '6085551023'),
+(2, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie', 'USA', 'Wisconsin', '6085551749'),
+(3, 'Eduardo', 'Rodriquez', '2693 Commerce St.', 'McFarland', 'USA', 'Wisconsin', '6085558763'),
+(4, 'Harold', 'Davis', '563 Friendly St.', 'Eugene', 'USA', 'Oregón', '6085553198'),
+(5, 'Peter', 'McTavish', '2387 S. Fair Way', 'Madison', 'USA', 'Wisconsin', '6085552765'),
+(6, 'Jean', 'Coleman', '105 N. Lake St.', 'Mundelein', 'USA', 'Illinois', '6085552654'),
+(7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', 'USA', 'Lowa', '6085555387'),
+(8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', 'USA', 'Tennesee', '6085557683'),
+(9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', 'USA', 'Wisconsin', '6085559435'),
+(10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', 'USA', 'Wisconsin', '6085555487');
+
+--
+-- Volcado de datos para la tabla `pets`
+--
+
+INSERT IGNORE INTO `pets` (`id`, `name`, `birth_date`, `type_id`, `owner_id`) VALUES
+(1, 'Leo', '2000-09-07', 1, 1),
+(2, 'Basil', '2002-08-06', 6, 2),
+(3, 'Rosy', '2001-04-17', 2, 3),
+(4, 'Jewel', '2000-03-07', 2, 3),
+(5, 'Iggy', '2000-11-30', 3, 4),
+(6, 'George', '2000-01-20', 4, 5),
+(7, 'Samantha', '1995-09-04', 1, 6),
+(8, 'Max', '1995-09-04', 1, 6),
+(9, 'Lucky', '1999-08-06', 5, 7),
+(10, 'Mulligan', '1997-02-24', 2, 8),
+(11, 'Freddy', '2000-03-09', 5, 9),
+(12, 'Lucky', '2000-06-24', 2, 10),
+(13, 'Sly', '2002-06-08', 1, 10);
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT IGNORE INTO `producto` (`Id`, `nombre`, `descripcion`, `numero_serie`, `cantidad`, `precio`, `file`) VALUES
+(1, 'Gasas', 'Material de curacion para las mascotas.', '12x23bqw2', 12, 34, NULL);
+
+--
+-- Volcado de datos para la tabla `specialties`
+--
+
+INSERT IGNORE INTO `specialties` (`id`, `name`) VALUES
+(3, 'dentistry'),
+(1, 'radiology'),
+(2, 'surgery');
+
+--
+-- Volcado de datos para la tabla `types`
+--
+
+INSERT IGNORE INTO `types` (`id`, `name`) VALUES
+(5, 'bird'),
+(1, 'cat'),
+(2, 'dog'),
+(6, 'hamster'),
+(3, 'lizard'),
+(4, 'snake');
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT IGNORE INTO `usuarios` (`idusuarios`, `username`, `password`, `email`, `enabled`, `codigopostal`) VALUES
+(1, 'david', '$2a$10$uxUu14ZltBy/Y9K9ofNt3.hb5IJtUTyxZCOabIqRerEXGm9Ku5agy', 'playeando.97@gmail.com', 1, '29100'),
+(2, 'admin', '$2a$10$uxUu14ZltBy/Y9K9ofNt3.hb5IJtUTyxZCOabIqRerEXGm9Ku5agy', 'admin@admin.com', 1, '29100');
+
+--
+-- Volcado de datos para la tabla `vets`
+--
+
+INSERT IGNORE INTO `vets` (`id`, `first_name`, `last_name`, `telephone`, `Schedule`, `specialty_id`) VALUES
+(2, 'Helen', 'Leary', '961 445 74', 'Fin de semanas', '1'),
+(3, 'Linda', 'Douglas', '961 121 23', 'De lunes a sabado', '3'),
+(4, 'Rafael', 'Ortega', '961 456 65', 'Toda la semana', '3'),
+(5, 'Henry', 'Stevens', '961 899 65', '7 dias y 24 horas', '2'),
+(6, 'Sharon', 'Jenkins', '961 002 12', 'Lunes, miercoles y viernes', '1');
+
+--
+-- Volcado de datos para la tabla `vet_specialties`
+--
+
+INSERT IGNORE INTO `vet_specialties` (`vet_id`, `specialty_id`) VALUES
+(2, 1),
+(3, 2),
+(3, 3),
+(4, 2),
+(5, 1);
+
+--
+-- Volcado de datos para la tabla `visits`
+--
+
+INSERT IGNORE INTO `visits` (`id`, `pet_id`, `visit_date`, `description`) VALUES
+(1, 7, '2010-03-04', 'rabies shot'),
+(2, 8, '2011-03-04', 'rabies shot'),
+(3, 8, '2009-06-04', 'neutered'),
+(4, 7, '2008-09-04', 'spayed');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/main/resources/db/mysql/database_structure.sql
@@ -0,0 +1,315 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-02-2019 a las 07:15:02
+-- Versión del servidor: 10.1.34-MariaDB
+-- Versión de PHP: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `petclinic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `authorities`
+--
+-- Creación: 24-01-2019 a las 17:39:02
+--
+
+CREATE TABLE IF NOT EXISTS `authorities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `authority` varchar(45) DEFAULT 'USER',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELACIONES PARA LA TABLA `authorities`:
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `log`
+--
+-- Creación: 31-01-2019 a las 18:01:13
+--
+
+CREATE TABLE IF NOT EXISTS `log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `informacion` varchar(255) COLLATE latin1_spanish_ci DEFAULT '   ',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- RELACIONES PARA LA TABLA `log`:
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicamento`
+--
+-- Creación: 24-01-2019 a las 17:39:03
+--
+
+CREATE TABLE IF NOT EXISTS `medicamento` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) NOT NULL,
+  `ingrediente_activo` varchar(40) NOT NULL,
+  `presentacion` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELACIONES PARA LA TABLA `medicamento`:
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `owners`
+--
+-- Creación: 02-02-2019 a las 06:02:30
+--
+
+CREATE TABLE IF NOT EXISTS `owners` (
+  `id` int(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) DEFAULT NULL,
+  `last_name` varchar(30) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(80) DEFAULT NULL,
+  `country` varchar(80) DEFAULT NULL,
+  `estado` varchar(80) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `last_name` (`last_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELACIONES PARA LA TABLA `owners`:
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pets`
+--
+-- Creación: 02-02-2019 a las 06:01:55
+--
+
+CREATE TABLE IF NOT EXISTS `pets` (
+  `id` int(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `type_id` int(4) UNSIGNED NOT NULL,
+  `owner_id` int(4) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`),
+  KEY `owner_id` (`owner_id`),
+  KEY `type_id` (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELACIONES PARA LA TABLA `pets`:
+--   `owner_id`
+--       `owners` -> `id`
+--   `type_id`
+--       `types` -> `id`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+-- Creación: 02-02-2019 a las 05:51:06
+--
+
+CREATE TABLE IF NOT EXISTS `producto` (
+  `Id` int(5) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `numero_serie` varchar(50) NOT NULL,
+  `cantidad` int(8) NOT NULL,
+  `precio` float(10,0) NOT NULL,
+  `file` text,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELACIONES PARA LA TABLA `producto`:
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `specialties`
+--
+-- Creación: 24-01-2019 a las 17:39:03
+--
+
+CREATE TABLE IF NOT EXISTS `specialties` (
+  `id` int(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELACIONES PARA LA TABLA `specialties`:
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `types`
+--
+-- Creación: 24-01-2019 a las 17:39:03
+--
+
+CREATE TABLE IF NOT EXISTS `types` (
+  `id` int(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELACIONES PARA LA TABLA `types`:
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+-- Creación: 31-01-2019 a las 05:03:26
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `idusuarios` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `enabled` int(11) DEFAULT '1',
+  `codigopostal` varchar(10) NOT NULL,
+  PRIMARY KEY (`idusuarios`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELACIONES PARA LA TABLA `usuarios`:
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vets`
+--
+-- Creación: 24-01-2019 a las 17:39:04
+--
+
+CREATE TABLE IF NOT EXISTS `vets` (
+  `id` int(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) DEFAULT NULL,
+  `last_name` varchar(30) DEFAULT NULL,
+  `telephone` varchar(10) NOT NULL,
+  `Schedule` varchar(100) NOT NULL,
+  `specialty_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `last_name` (`last_name`),
+  KEY `specialty` (`specialty_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELACIONES PARA LA TABLA `vets`:
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vet_specialties`
+--
+-- Creación: 24-01-2019 a las 17:39:04
+--
+
+CREATE TABLE IF NOT EXISTS `vet_specialties` (
+  `vet_id` int(4) UNSIGNED NOT NULL,
+  `specialty_id` int(4) UNSIGNED NOT NULL,
+  UNIQUE KEY `vet_id` (`vet_id`,`specialty_id`),
+  KEY `specialty_id` (`specialty_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELACIONES PARA LA TABLA `vet_specialties`:
+--   `vet_id`
+--       `vets` -> `id`
+--   `specialty_id`
+--       `specialties` -> `id`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `visits`
+--
+-- Creación: 24-01-2019 a las 17:39:04
+--
+
+CREATE TABLE IF NOT EXISTS `visits` (
+  `id` int(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `pet_id` int(4) UNSIGNED NOT NULL,
+  `visit_date` date DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pet_id` (`pet_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELACIONES PARA LA TABLA `visits`:
+--   `pet_id`
+--       `pets` -> `id`
+--
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pets`
+--
+ALTER TABLE `pets`
+  ADD CONSTRAINT `pets_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `owners` (`id`),
+  ADD CONSTRAINT `pets_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `types` (`id`);
+
+--
+-- Filtros para la tabla `vet_specialties`
+--
+ALTER TABLE `vet_specialties`
+  ADD CONSTRAINT `vet_specialties_ibfk_1` FOREIGN KEY (`vet_id`) REFERENCES `vets` (`id`),
+  ADD CONSTRAINT `vet_specialties_ibfk_2` FOREIGN KEY (`specialty_id`) REFERENCES `specialties` (`id`);
+
+--
+-- Filtros para la tabla `visits`
+--
+ALTER TABLE `visits`
+  ADD CONSTRAINT `visits_ibfk_1` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
