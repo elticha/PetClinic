9be2e4970239a6817d9ede53b8f903ab02ba51bb
Ingeniero Fernando <163189@ids.upchiapas.edu.mx>
2019-02-05 12:03:16 -0600

    Creación de clase que registra intentos fallidpos de login
    .......

---
+++ src/main/java/org/springframework/samples/petclinic/FailedAuthListener.java
@@ -0,0 +1,30 @@
+package org.springframework.samples.petclinic;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.samples.petclinic.system.Log;
+import org.springframework.samples.petclinic.system.LogRepository;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Fernando
+ */
+@Component
+public class FailedAuthListener {
+    
+    @Autowired
+    private LogRepository lr;
+    
+    @EventListener
+    public void authenticationFailed(AuthenticationFailureBadCredentialsEvent e)
+    {
+        String username = (String) e.getAuthentication().getPrincipal();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        this.lr.save(new Log("[ Error de credenciales ] El usuario " + username + " ha intentado acceder a las " + dtf.format(now)));
+    }
+}
