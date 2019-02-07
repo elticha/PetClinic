8cb483bc78460cd66a8c942c9b61d24397861f83
Raymond981 <163163@ids.upchiapas.edu.mx>
2019-02-07 15:05:00 -0600

    Actualizacion de ultima hora

--- src/main/java/org/springframework/samples/petclinic/FailedAuthListener.java
+++ src/main/java/org/springframework/samples/petclinic/FailedAuthListener.java
@@ -7,6 +7,7 @@ import org.springframework.context.event.EventListener;
 import org.springframework.samples.petclinic.system.Log;
 import org.springframework.samples.petclinic.system.LogRepository;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.authentication.event.AuthenticationFailureDisabledEvent;
 import org.springframework.stereotype.Component;
 
 /**
@@ -20,9 +21,30 @@ public class FailedAuthListener {
     private LogRepository lr;
     
     @EventListener
+    public void authenticationDisabledFailed(AuthenticationFailureDisabledEvent e)
+    {
+        System.out.println("Entrandoooooo a la seccion disabled");
+        String username = (String) e.getAuthentication().getPrincipal();
+        System.out.println(username);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        try
+        {
+            System.out.println("Guardando...");
+            this.lr.save(new Log("[ Error de credenciales ] El usuario " + username + " [disabled] ha intentado acceder a las " + dtf.format(now)));
+        }
+        catch(Exception er)
+        {
+            er.printStackTrace();
+        }
+    }
+    
+    @EventListener
     public void authenticationFailed(AuthenticationFailureBadCredentialsEvent e)
     {
+        System.out.println("Entrandoooooo");
         String username = (String) e.getAuthentication().getPrincipal();
+        System.out.println(username);
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         try
